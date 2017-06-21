53d3dae99e66dd81e243c07c55f517a101433df6
LAPTOP-204T8SBM\takuma_baba <pppp1219pppp@yahoo.co.jp>
2017-06-21 13:51:50 +0900

    【追加】　メインクラスを追加

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace WpfApp1
+﻿namespace WpfApp1
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
@@ -22,7 +22,24 @@ namespace WpfApp1
     {
         public MainWindow()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+
+            var program = new Program
+            {
+                Block = this.CreateList(),
+            };
+
+            this.DataContext = program;
+        }
+
+        public int[] CreateList()
+        {
+            int[] block = new int[11];
+            for (int i = 1; i <= 10; i++)
+            {
+                 block[i] = i;
+            }
+            return block;
         }
     }
 }
---
+++ WpfApp1/WpfApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace WpfApp1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+
+        private int[] block;
+
+
+        public string Id { get; set; }
+
+        public int[] Block { get; set; }
+
+    }
+}
