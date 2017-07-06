beb097951292b2fc6b7037b050d580397c383f8a
LAPTOP-204T8SBM\takuma_baba <pppp1219pppp@yahoo.co.jp>
2017-07-07 08:59:01 +0900

    :pencil2: Dataテンプレートのkeyの削除
    動作不良の原因は不明

--- WpfApp1/WpfApp1/CommonCalenderCreate.cs
+++ WpfApp1/WpfApp1/CommonCalenderCreate.cs
@@ -16,6 +16,16 @@ namespace WpfApp1
     using CustomEventArgs;
 
     /// <summary>
+    /// DayのPath用enum
+    /// </summary>
+    internal enum DayPath
+    {
+        Day1, Day2, Day3, Day4, Day5, Day6, Day7, Day8, Day9, Day10,
+        Day11, Day12, Day13, Day14, Day15, Day16, Day17, Day18, Day19, Day20,
+        Day21, Day22, Day23, Day24, Day25, Day26, Day27, Day28, Day29, Day30, Day31
+    }
+
+    /// <summary>
     /// イベントハンドラーの設定
     /// </summary>
     /// <param name="sender">クラス情報</param>
@@ -172,6 +182,11 @@ namespace WpfApp1
             get { return this.entity; }
             set { this.entity = value; }
         }
+
+        //public int DayPathProp
+        //{
+        //   // get { return DayPath.; }
+        //}
 
         /// <summary>
         /// 曜日のチェックラベルの変更用メソッド
